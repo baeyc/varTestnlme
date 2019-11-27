381f6d125922745f5e2022bb5dfa1bcc67c695f7
Charlotte Baey <charlotte.baey@gmail.com>
2019-11-27 23:12:55 +0100

    add plot, print and summary for varTestObject

---
+++ R/print.varTestObject.R
@@ -0,0 +1,28 @@
+##' @name print.varTestObject
+##' @rdname print.varTestObject
+##'
+##' @title Print basic information about the variance components test
+##'
+##' @description Displays the likelihood ratio test statistics and the p-value of the test
+##'
+##' @param x a object of class \code{\link{varTestObject}} obtained from a call to function \code{\link{varTest}}
+##'
+NULL
+
+print.varTestObject <- function(x){
+  if (length(x@chibarsquare) == 0){
+    if (length(x@pvalueWeights) == 0){
+      uppboundpval <- (1/2)*sum(stats::pchisq(lrt,x@df[(length(x@df)-1):length(x@df)],lower.tail = F))
+      lowboundpval <- (1/2)*sum(stats::pchisq(lrt,x@df[1:2],lower.tail = F))
+      cat("Variance components testing\n LRT = ",x@lrt,
+          paste("\n\nlower-bound for p-value:",format(lowboundpval,digits=5)," upper bound for p-value:",format(uppboundpval,digits=5)))
+    }else{
+      cat("Variance components testing\n LRT = ",x@lrt,
+          "\n\np-value (from estimated weights):",format(x@pvalueWeights,digits = 5))
+    }
+  }else{
+    cat("Variance components testing\n LRT = ",x@lrt,
+        "\n\np-value (from estimated weights):",format(x@pvalueWeights,digits = 5),
+        "\np-value (from simulated chi-bar-square distribution):",format(x@pvalueWeights,digits = 5))
+  }
+}
